1f60597e5f7f488c0923eee84b1f7cf6c8c5037c
Mateusz Najdek <mateusz@najdek.me>
2024-02-08 00:41:17 +0100

    Add theater mode button image

--- f1tv_plus.user.js
+++ f1tv_plus.user.js
@@ -12,8 +12,9 @@
 var DEFAULT_THEATERMODE = true;
 
 
-
+var theatermode_btn_image = "data:image/svg+xml;base64,PHN2ZyB2ZXJzaW9uPScxLjEnIGlkPSdMYXllcl8xJyB4bWxucz0naHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmcnIHhtbG5zOnhsaW5rPSdodHRwOi8vd3d3LnczLm9yZy8xOTk5L3hsaW5rJyB4PScwcHgnIHk9JzBweCcgdmlld0JveD0nMCAwIDI4My41IDQyNS4yJyBzdHlsZT0nZW5hYmxlLWJhY2tncm91bmQ6bmV3IDAgMCAyODMuNSA0MjUuMjsnIHhtbDpzcGFjZT0ncHJlc2VydmUnPjxnPjxwYXRoIHN0eWxlPSdmaWxsOiNEMEQwRDI7JyBkPSdNMjM5LjMsMTI0LjlINDQuMmMtMTAuNywwLTE5LjQsOC43LTE5LjQsMTkuNHYxMzYuNmMwLDEwLjcsOC43LDE5LjQsMTkuNCwxOS40aDE5NS4xICBjMTAuNywwLDE5LjQtOC43LDE5LjQtMTkuNFYxNDQuM0MyNTguNywxMzMuNiwyNTAsMTI0LjksMjM5LjMsMTI0Ljl6IE04Ni43LDI4Ny40SDM4di00OC43aDE5LjV2MjkuMmgyOS4yVjI4Ny40eiBNODYuNywxNTYuMiBINTcuNXYyOS4ySDM4di00OC43aDQ4LjdWMTU2LjJ6IE0yNDUuNSwyODcuNGgtNDguN3YtMTkuNUgyMjZ2LTI5LjJoMTkuNVYyODcuNHogTTI0NS41LDE4NS40SDIyNnYtMjkuMmgtMjkuMnYtMTkuNWg0OC43VjE4NS40eicvPjwvZz48L3N2Zz4NCg==";
 var theatermode_active = DEFAULT_THEATERMODE;
+
 function log(msg) {
   var d = new Date();
   var dt = d.toISOString().split('T')[0] + " " + d.toTimeString().split(' ')[0];
@@ -70,7 +71,7 @@ function injectPlayerFeatures() {
   }
 
   // add theater mode button
-  var theaterBtnHtml = "<button aria-label='Theater mode' class='f1tvplus-btn-theatermode bmpui-off' type='button' aria-pressed='false' tabindex='0' role='button'><span class='bmpui-label'>Theater mode</span></button>";
+  var theaterBtnHtml = "<button aria-label='Theater mode' class='f1tvplus-btn-theatermode bmpui-off' style='background-color: transparent; background-origin: content-box; background-position: center; background-repeat: no-repeat; background-size: 1.5em; border: 0; -webkit-box-sizing: content-box; box-sizing: content-box; cursor: pointer; font-size: 1em; height: 1.5em; min-width: 1.5em; padding: 0.25em; background-image: url(" + theatermode_btn_image + ")' type='button' aria-pressed='false' tabindex='0' role='button'><span class='bmpui-label' style='display: none;'>Theater mode</span></button>";
   $(".f1tvplus-player .bmpui-container-wrapper .bmpui-ui-piptogglebutton")[0].insertAdjacentHTML("beforebegin", theaterBtnHtml);
   $(".f1tvplus-player .f1tvplus-btn-theatermode").on("click", function(event) {
     toggleTheaterMode();
@@ -81,8 +82,6 @@ function injectPlayerFeatures() {
     toggleTheaterMode();
   }
 }
-
-
 
 function waitForPlayer() {
   if ($(".player-container.shown .bitmovinplayer-container").length > 0) {
@@ -101,17 +100,16 @@ function waitForPlayer() {
   }
 }
 
-
 function waitForPageLoad() {
   if ($("#app .app-wrapper").length > 0) {
     clearInterval(waitForPageLoad);
     $("#app")[0].insertAdjacentHTML("beforeEnd", "<div id='f1tvplus-helper'></div>");
   }
 }
+
 var waitForPageLoad = setInterval(waitForPageLoad, 50);
-
-
 var playerInjectLoop;
+
 function pageChanged() {
   clearInterval(playerInjectLoop);
   if (document.location.href.split("formula1.com/")[1].split("/")[0] == "detail") {
@@ -122,7 +120,6 @@ function pageChanged() {
   }
 }
 
-
 var oldHref = document.location.href;
 var observer = new MutationObserver(function(mutations) {
     mutations.forEach(function(mutation) {
@@ -132,6 +129,7 @@ var observer = new MutationObserver(function(mutations) {
         }
     });
 });
+
 observer.observe(document.querySelector("body"), {
     childList: true,
     subtree: true
