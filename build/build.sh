eaf9a2209e3b3f850d58b4a302127657df66441e
Mateusz Najdek <mateusz@najdek.me>
2025-03-15 01:28:29 +0100

    Fix build script for macOS compatibility

--- build/build.sh
+++ build/build.sh
@@ -19,9 +19,9 @@ cd tmp/extension
 
 VERSION=`grep "// @version" carbon-for-f1tv.user.js | rev | cut -d ' ' -f 1 | rev | tr -d '\n'`
 echo $VERSION
-sed -i "s/0.1000.1000/$VERSION/" manifest.json
-sed -i "s/0.1000.1000/$VERSION/" manifest_firefox.json
-sed -i "s/0.1000.1000/$VERSION/" popup.html
+sed -i "" "s/0.1000.1000/$VERSION/" manifest.json
+sed -i "" "s/0.1000.1000/$VERSION/" manifest_firefox.json
+sed -i "" "s/0.1000.1000/$VERSION/" popup.html
 
 
 mv manifest_firefox.json background_firefox.js load_firefox.js ../
